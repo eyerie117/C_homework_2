c4728de7a1efc9d98c813e054f9dec29b9cf15e5
Kseniia Esepenko <eyerie86@gmail.com>
2023-09-25 16:54:25 +0300

    Task 2

--- Program.cs
+++ Program.cs
@@ -8,3 +8,17 @@
 // int x = new Random().Next(100, 1000);
 // Console.WriteLine(x);
 // Console.WriteLine((x / 10) % 10);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+int x = new Random().Next(0, 100000);
+string N = x.ToString();
+Console.WriteLine(x);
+
+if (x < 100) Console.WriteLine("Третьей цифры нет");
+else Console.WriteLine(N[2]);
