260e119d165736b6ad55ec7bfc1702621bf1687b
Kseniia Esepenko <eyerie86@gmail.com>
2023-09-25 17:12:15 +0300

    Task 3

--- Program.cs
+++ Program.cs
@@ -14,11 +14,25 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Console.Clear();
+
+// int x = new Random().Next(0, 100000);
+// string N = x.ToString();
+// Console.WriteLine(x);
+
+// if (x < 100) Console.WriteLine("Третьей цифры нет");
+// else Console.WriteLine(N[2]);
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
 Console.Clear();
 
-int x = new Random().Next(0, 100000);
-string N = x.ToString();
-Console.WriteLine(x);
+Console.Write("Введите порядковый номер дня недели: ");
+int i = Convert.ToInt32(Console.ReadLine());
 
-if (x < 100) Console.WriteLine("Третьей цифры нет");
-else Console.WriteLine(N[2]);
+if (i == 6 || i == 7) Console.WriteLine("Да");
+else if (i < 1 || i > 7) Console.WriteLine("Вы ввели неверные данные");
+else Console.WriteLine("Нет");
